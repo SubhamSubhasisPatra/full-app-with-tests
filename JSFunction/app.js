9ed6f38a239485394fbf1e8db1da0035d0db8747
subhasis <subhasis@subhasiss-MacBook-Air.local>
2021-07-24 17:38:42 +0530

    added the Factory Function

--- JSFunction/app.js
+++ JSFunction/app.js
@@ -16,3 +16,20 @@ let showHobbies = (h) => {
 
 showHobbies("reading");
 showHobbies("edting");
+
+// Factory function
+// if the function return a new object or a new Function, it is a factory function
+
+let createTaxCalculator = (tax) => {
+  function calculateAmount(amount) {
+    return tax * amount;
+  }
+
+  return calculateAmount;
+};
+
+let vatTax = createTaxCalculator(0.12);
+let incomeTax = createTaxCalculator(0.18);
+
+console.log(vatTax(100));
+console.log(incomeTax(100));
